1fe5b95240cfb31652a853b51e9cdaaa1212b739
tangjiujun <jiujun.tang@thoughtworks.com>
2024-04-12 14:24:34 +0800

    update start.sh script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 docker build -t dify-demo:latest .
 docker rm -f dify-demo
 docker run -d --name dify-demo -p 3000:3000 dify-demo:latest
+
+# docker run -d --name nginx -p 443:443 -v /root/nginx/conf:/etc/nginx/conf.d -v /root/nginx/ssl:/etc/nginx/ssl nginx
